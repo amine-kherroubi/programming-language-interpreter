dca4c9706233dea6c29a476bc4e7126a59dc1a32
Mohamed El Amine Kherroubi <elaminekherroubi@gmail.com>
2025-07-26 22:10:45 +0100

    Add support for arbitrary number of multiplications, divisions, additions, and substractions, with precedence

--- calc1.py
+++ calc1.py
@@ -1,51 +1,81 @@
 from typing import Optional, Union, NoReturn
 
-INTEGER, OP, EOF = "INTEGER", "OP", "EOF"
-OPS = ["+", "-", "*", "/"]
+# Token types
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = "INTEGER", "PLUS", "MINUS", "MUL", "DIV", "EOF"
 
 
 class Token:
+    __slots__ = ("type", "value")
+
     def __init__(self, type: str, value: Optional[Union[int, str]]) -> None:
         self.type: str = type
         self.value: Optional[Union[int, str]] = value
 
     def __str__(self) -> str:
-        return f"Token({self.type}, {self.value})"
+        return f"Token({self.type}, {self.value!r})"
 
     def __repr__(self) -> str:
         return self.__str__()
 
 
 class Interpreter:
+    __slots__ = ("text", "pos", "current_token", "current_char")
+
     def __init__(self, text: str) -> None:
         self.text: str = text
         self.pos: int = 0
         self.current_token: Optional[Token] = None
+        self.current_char: Optional[str] = self.text[self.pos] if self.text else None
 
     def error(self) -> NoReturn:
         raise Exception("Error parsing input")
 
+    def advance(self) -> None:
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self) -> None:
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self) -> int:
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
     def get_next_token(self) -> Token:
-        while self.pos < len(self.text) and self.text[self.pos] == " ":
-            self.pos += 1
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        if self.pos >= len(self.text):
-            return Token(EOF, None)
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
 
-        current_char: str = self.text[self.pos]
+            if self.current_char == "+":
+                self.advance()
+                return Token(PLUS, "+")
 
-        if current_char.isdigit():
-            integer_str: str = ""
-            while self.pos < len(self.text) and self.text[self.pos].isdigit():
-                integer_str += self.text[self.pos]
-                self.pos += 1
-            return Token(INTEGER, int(integer_str))
+            if self.current_char == "-":
+                self.advance()
+                return Token(MINUS, "-")
 
-        if current_char in OPS:
-            self.pos += 1
-            return Token(OP, current_char)
+            if self.current_char == "*":
+                self.advance()
+                return Token(MUL, "*")
 
-        self.error()
+            if self.current_char == "/":
+                self.advance()
+                return Token(DIV, "/")
+
+            self.error()
+
+        return Token(EOF, None)
 
     def eat(self, token_type: str) -> None:
         if self.current_token and self.current_token.type == token_type:
@@ -53,47 +83,47 @@ class Interpreter:
         else:
             self.error()
 
-    def apply_operation(
-        self, left: Union[int, float], op: Token, right: Union[int, float]
-    ) -> Union[int, float]:
-        if op.value == "+":
-            return left + right
-        elif op.value == "-":
-            return left - right
-        elif op.value == "*":
-            return left * right
-        elif op.value == "/":
-            if right == 0:
-                raise Exception("Division by zero")
-            return left / right
-        raise Exception("Wrong token value types")
+    def factor(self) -> int:
+        token: Optional[Token] = self.current_token
+        self.eat(INTEGER)
+        if token and isinstance(token.value, int):
+            return token.value
+        self.error()
+
+    def term(self) -> Union[int, float]:
+        result = self.factor()
+
+        while self.current_token and self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
+                result = result * self.factor()
+            elif token.type == DIV:
+                self.eat(DIV)
+                divisor = self.factor()
+                if divisor == 0:
+                    raise Exception("Division by zero")
+                result = result / divisor
+
+        return result
 
     def expr(self) -> Union[int, float]:
         self.current_token = self.get_next_token()
-        left: Token = self.current_token
-        self.eat(INTEGER)
-        op: Token = self.current_token
-        self.eat(OP)
-        right: Token = self.current_token
-        self.eat(INTEGER)
-        result: Union[int, float]
-        if (
-            isinstance(left.value, int)
-            and isinstance(right.value, int)
-            and isinstance(op.value, str)
-        ):
-            result = self.apply_operation(left.value, op, right.value)
-        else:
-            raise Exception("Invalid token value type")
-        while self.pos < len(self.text):
-            op: Token = self.current_token
-            self.eat(OP)
-            right: Token = self.current_token
-            self.eat(INTEGER)
-            if isinstance(right.value, int) and isinstance(op.value, str):
-                result = self.apply_operation(result, op, right.value)
+
+        result = self.term()
+
+        while self.current_token and self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+
         if self.current_token and self.current_token.type != EOF:
             self.error()
+
         return result
 
 
@@ -105,8 +135,8 @@ def main() -> None:
             break
         if not text:
             continue
-        interpreter: Interpreter = Interpreter(text)
-        result: Union[int, float] = interpreter.expr()
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
         print(result)
 
 
